9e42ff1f35272612d75b8b0f72ffc840dec662bb
Midhun Babu <Midhun@qbadmins-iMac-103.local>
2017-02-13 21:17:21 +0530

    Restaurant Detais

--- Restaurant_Finder_App/restaurant_finder_app/restaurant_finder_app/restaurant/static/restaurant_detail/js/click.js
+++ Restaurant_Finder_App/restaurant_finder_app/restaurant_finder_app/restaurant/static/restaurant_detail/js/click.js
@@ -6,3 +6,32 @@ $(document).ready(function(){
         closeEffect: "none"
     });
 });
+
+$(function(){
+  $('.lightbox').click(function(event){
+    event.preventDefault();
+    // need to be able to get the link target
+    $link = $(event.target).closest('a');
+
+    $.getJSON($link.attr('href'), function(menu_image){
+      // map the json to an array that fancybox can use
+      images = $(menu_image.images).map(function(key, image)){
+        return({ href: image.url });
+      });
+
+      console.log(images);
+      // => [{href: 'image1.jpg'},{href: 'image2.jpg'},{href: 'image3.jpg'}]
+
+      // show the lightbox
+      $.fancybox.open(images);
+    });
+  });
+});
+
+$("#triggerGallery").click(function (e) {
+    e.preventDefault();
+    $(".images").eq(0).trigger("click");
+})
+$(".grouped_elements").fancybox();
+
+
